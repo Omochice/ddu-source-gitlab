19cb893c6ffc484f57e5911db0781031a7108dc5
Omochice <mochice.mls.ntl@gmail.com>
2025-05-26 13:19:14 +0900

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "GitLab sources for ddu.vim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nur = {
+      url = "github:Omochice/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      flake-utils,
+      nur,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nur.overlays.default ];
+        };
+        treefmt = treefmt-nix.lib.evalModule pkgs (
+          { ... }:
+          {
+            settings.global.excludes = [
+              "CHANGELOG.md"
+            ];
+            programs = {
+              # keep-sorted start block=yes
+              deno.enable = true;
+              keep-sorted.enable = true;
+              mdformat.enable = true;
+              nixfmt.enable = true;
+              yamlfmt = {
+                enable = true;
+                settings = {
+                  formatter = {
+                    type = "basic";
+                    retain_line_breaks_single = true;
+                  };
+                };
+              };
+            };
+            # keep-sorted end
+          }
+        );
+      in
+      {
+        formatter = treefmt.config.build.wrapper;
+        checks = {
+          formatting = treefmt.config.build.check self;
+        };
+      }
+    );
+}
